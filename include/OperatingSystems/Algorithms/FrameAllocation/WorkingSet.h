49102f788ab1cf1c15b38258c17fe38b6f6b8a47
Tomasz Durda (Dzordzu) <tomekdur@wp.pl>
2019-05-17 20:17:25 +0000

    Added working set

---
+++ include/OperatingSystems/Algorithms/FrameAllocation/WorkingSet.h
@@ -0,0 +1,26 @@
+//
+// Created by dzordzu on 5/17/19.
+//
+
+#ifndef OPERATINGSYSTEMS_ALGORITHMS_FRAMEALLOCATION_WORKINGSET_H
+#define OPERATINGSYSTEMS_ALGORITHMS_FRAMEALLOCATION_WORKINGSET_H
+
+#include <vector>
+#include <functional>
+#include "Algorithm.h"
+#include <OperatingSystems/Processor/Processor.h>
+
+namespace OperatingSystems {
+    namespace Algorithms {
+        namespace FrameAllocation {
+            class WorkingSet : public Algorithm {
+                std::vector<std::reference_wrapper<Page>> recentPages;
+            public:
+                void allocateFrames() override;
+            };
+        }
+    }
+}
+
+
+#endif //OPERATINGSYSTEMS_ALGORITHMS_FRAMEALLOCATION_WORKINGSET_H
---
+++ src/Algorithms/FrameAllocation/WorkingSet.cpp
@@ -0,0 +1,9 @@
+//
+// Created by dzordzu on 5/17/19.
+//
+
+#include "OperatingSystems/Algorithms/FrameAllocation/WorkingSet.h"
+
+void OperatingSystems::Algorithms::FrameAllocation::WorkingSet::allocateFrames() {
+
+}

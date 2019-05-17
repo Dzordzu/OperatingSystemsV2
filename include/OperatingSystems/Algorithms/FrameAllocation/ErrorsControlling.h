da667f6e1ae49a3eceaa59011fce6b7550a6ae71
Tomasz Durda (Dzordzu) <tomekdur@wp.pl>
2019-05-17 22:54:10 +0000

    Added ErrorsControlling algo

---
+++ include/OperatingSystems/Algorithms/FrameAllocation/ErrorsControlling.h
@@ -0,0 +1,22 @@
+//
+// Created by dzordzu on 5/17/19.
+//
+
+#ifndef OPERATINGSYSTEMS_ALGORITHMS_FRAMEALLOCATION_ERRORSCONTROLLING_H
+#define OPERATINGSYSTEMS_ALGORITHMS_FRAMEALLOCATION_ERRORSCONTROLLING_H
+
+#include "Algorithm.h"
+#include <OperatingSystems/Processor/Processor.h>
+
+namespace OperatingSystems {
+    namespace Algorithms {
+        namespace FrameAllocation {
+            class ErrorsControlling {
+
+            };
+        }
+    }
+}
+
+
+#endif //OPERATINGSYSTEMS_ALGORITHMS_FRAMEALLOCATION_ERRORSCONTROLLING_H
---
+++ src/Algorithms/FrameAllocation/ErrorsControlling.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dzordzu on 5/17/19.
+//
+
+#include "OperatingSystems/Algorithms/FrameAllocation/ErrorsControlling.h"

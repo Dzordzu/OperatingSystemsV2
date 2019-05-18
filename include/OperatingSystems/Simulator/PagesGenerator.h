f10a47b5e67501ac95e717f40f60167d4cc4f66d
Tomasz Durda (Dzordzu) <tomekdur@wp.pl>
2019-05-18 02:41:39 +0000

    Added PagesGenerator

---
+++ include/OperatingSystems/Simulator/PagesGenerator.h
@@ -0,0 +1,24 @@
+//
+// Created by dzordzu on 5/18/19.
+//
+
+#ifndef OPERATINGSYSTEMS_SIMULATOR_PAGESGENERATOR_H
+#define OPERATINGSYSTEMS_SIMULATOR_PAGESGENERATOR_H
+
+#include <string>
+#include <OperatingSystems/Processor/Processor.h>
+
+namespace OperatingSystems {
+    namespace Simulator {
+
+
+        class PagesGenerator {
+        protected:
+
+        };
+
+    }
+}
+
+
+#endif //OPERATINGSYSTEMS_SIMULATOR_PAGESGENERATOR_H
---
+++ src/Simulator/PagesGenerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dzordzu on 5/18/19.
+//
+
+#include "OperatingSystems/Simulator/PagesGenerator.h"

69bc6c680d5c5113a21014b96ae6a6ce73016dcd
Tomasz Durda (Dzordzu) <tomekdur@wp.pl>
2019-05-18 00:26:51 +0000

    Added ErrorsCounter

---
+++ include/OperatingSystems/Processor/ErrorsCounter.h
@@ -0,0 +1,25 @@
+//
+// Created by dzordzu on 5/18/19.
+//
+
+#ifndef OPERATINGSYSTEMSS_PROCESSOR_ERRORSCOUNTER_H
+#define OPERATINGSYSTEMSS_PROCESSOR_ERRORSCOUNTER_H
+
+#include <cstdint>
+#include <string>
+
+namespace OperatingSystems {
+    namespace Processor {
+        class ErrorCounter {
+            std::string counterName;
+            uint_fast64_t errors = 0;
+        public:
+            explicit ErrorCounter(const std::string &counterName) : counterName(counterName) {}
+            void add(int amount = 1) { errors+=amount; }
+            uint_fast64_t getErrors() const {return errors;}
+            void reset() {errors = 0;}
+        };
+    }
+}
+
+#endif //OPERATINGSYSTEMSS_PROCESSOR_ERRORSCOUNTER_H
